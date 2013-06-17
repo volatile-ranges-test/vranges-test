b9e4b1ce2dd8d74f1e1308da3187e4d16e9b8876
Dhaval Giani <dhaval.giani@gmail.com>
2013-06-17 18:34:49 -0400

    Add a script to run tests
    We still have to figure out how to check the status of
    the tests
    
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

---
+++ run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#We should be in the test directory at this point in time.
+
+echo "Current cgroup"
+cat /proc/self/cgroup | grep memory
+
+#Just add your test here, we will figure out how to check the
+#results later on.
+./volatile-test
+./volatile-test
+./volatile-test
+./volatile-test
+./volatile-test
+./volatile-test
