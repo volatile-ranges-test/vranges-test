ad1bb389d0a0a441ae493053256501e60927a801
Dhaval Giani <dhaval.giani@gmail.com>
2013-06-17 18:34:27 -0400

    Make setup.sh output more idiot-proof
    Signed-off-by: Dhaval Giani <dhaval.giani@gmail.com>

--- setup.sh
+++ setup.sh
@@ -6,3 +6,14 @@ echo $curr_user
 
 echo "Now we need root"
 sudo ./setup-cgroups.sh $curr_user
+
+echo $$ > /sys/fs/cgroup/memory/test/tasks
+
+echo "Now we are trapped in the correct cgroup"
+cat /proc/self/cgroup | grep memory
+
+echo "Running Tests"
+./run-tests.sh
+
+echo "Tearing down cgroups"
+sudo ./teardown-cgroup.sh
